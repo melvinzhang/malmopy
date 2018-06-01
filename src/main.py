1af5a1bef7cb93e0689df69516c91be7f3ac11df
melvinzhang <melvinzhang@gmail.com>
2018-06-01 20:10:04 +0800

    rename MalmoPy to malmopy

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-import MalmoPy
+import malmopy
 import time
 
 def setup_mission(spec):
     # with open('default.xml', 'r') as mfile:
-    #     spec = MalmoPython.MissionSpec(mfile.read(), True)
+    #     spec = malmopython.MissionSpec(mfile.read(), True)
     spec.setTimeOfDay(12000, False)
     spec.startAtWithPitchAndYaw(0.5, 227, 0.5, 0, 90)
     spec.drawSphere(-15, 227, 0, 10, 'air')
@@ -11,7 +11,7 @@ def setup_mission(spec):
     spec.allowAllDiscreteMovementCommands()
 
 if __name__ == "__main__":
-    malmo = MalmoPy.Malmo()
+    malmo = malmopy.Malmo()
     malmo.setup_mission(setup_mission)
     malmo.start_mission()
     for i in range(10):
