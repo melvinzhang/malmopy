2b1f41d913d6be504b82eccf530edd5519311eb9
melvinzhang <melvinzhang@gmail.com>
2018-06-02 21:52:51 +0800

    add maze solving agent using left hand rule

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ main:
 
 cliff_walking:
 	python3 src/$@.py
+
+maze:
+	python3 src/$@.py
---
+++ src/maze.py
@@ -0,0 +1,24 @@
+import malmopy
+
+malmo = malmopy.Malmo()
+malmo.set_delay(0.1)
+malmo.setup_mission('missions/maze_1.xml')
+malmo.start_mission()
+while malmo.is_running():
+    obs = malmo.observe()
+    if obs is None:
+        break
+    floor = obs['floor']
+    print(floor)
+    # left hand rule:
+    #   left side is empty, turn in
+    if floor[(-1,0)] == 'air':
+        malmo.turn(-1)
+        malmo.move(1)
+    # left is blocked, front is empty
+    elif floor[(0,1)] == 'air':
+        malmo.move(1)
+    # left is blocked, front is blocked
+    else:
+        malmo.turn(1)
+
